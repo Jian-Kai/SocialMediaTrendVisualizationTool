367bf8145806420a5ca1ef9b8ae185e66c9b36d0
tony14256 <tony14256@gmail.com>
2016-07-01 13:07:56 +0800

    ACGinti

---
+++ server.js
@@ -0,0 +1,21 @@
+// import modules
+var express                         = require('express'),				// npm install express
+ 	  app                             = express();
+
+app.get('/index', function(req, res)
+      {
+        console.log("hello");
+      });
+
+var port = process.env.PORT || 4000;
+
+app.listen(port, function() {
+	console.log("Express server listening on port %d", port);
+});
+
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/html/'));
+
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+});
