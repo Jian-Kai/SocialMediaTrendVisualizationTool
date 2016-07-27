ee04133a4608222c911bdc01a441f782ff548115
tony14256 <tony14256@gmail.com>
2016-07-27 17:27:46 +0800

    手指
    擴散移動可以在平板上操作
    hammer.js  一個手勢的函式庫

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 // import modules
-var express                         = require('express'),				// npm install express
- 	  app                             = express();
+var express = require('express'), // npm install express
+    app = express();
 
-app.get('/index', function(req, res)
-      {
-        console.log("hello");
-      });
+app.get('/index', function(req, res) {
+    console.log("hello");
+});
+app.get('/test', function(req, res) {
+    console.log("hello");
+});
+app.get('/ttttt', function(req, res) {
+    console.log("hello");
+});
+
+
 
 var port = process.env.PORT || 4000;
 
 app.listen(port, function() {
-	console.log("Express server listening on port %d", port);
+    console.log("Express server listening on port %d", port);
 });
 
 app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/html/'));
 
-process.on('uncaughtException', function (err) {
-  console.log('Caught exception: ' + err);
+process.on('uncaughtException', function(err) {
+    console.log('Caught exception: ' + err);
 });
