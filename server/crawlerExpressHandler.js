77e81676d926de0f73d8cb1eb8aac22855d668e2
tony14256 <tony14256@gmail.com>
2016-10-31 14:53:06 +0800

    撈reactions

--- server/crawlerExpressHandler.js
+++ server/crawlerExpressHandler.js
@@ -1,10 +1,10 @@
 async = require('async'),
     fs = require("fs"),
     graph = require("fbgraph");
-
+graph.setVersion("2.6");
 var savejson = function savejson(name, jsondata) {
     var Today = new Date();
-    var string = Today.getFullYear()+ "-" + (Today.getMonth()+1) + "-" + Today.getDate()
+    var string = Today.getFullYear() + "-" + (Today.getMonth() + 1) + "-" + Today.getDate()
     fs.writeFile("./textdata/" + name + "-" + string + ".json", JSON.stringify(jsondata));
 };
 
@@ -14,6 +14,7 @@ var save_photo_id = function save_photo_id(id) {
 };
 
 var callback = function callback(req, res) {
+
     var postid = req.query.postid;
     var token = req.query.token;
 
@@ -22,12 +23,55 @@ var callback = function callback(req, res) {
 
     var photo_id = 0;
 
+
     //query setting
     graph.setAccessToken(token)
-    var field_query = "?fields=id,object_id,type,message,story,from,shares,likes.limit(1).summary(true),comments.limit(1).summary(true)&since=2016-09-23&until=2016-10-25&limit=100";
+    var field_query = "?fields=id,object_id,created_time,type,message,story,from,shares,reactions.limit(1).summary(true),comments.limit(1).summary(true)&since=2016-09-23&until=2016-10-25&limit=100";
 
-    graph.get('/' + postid + '/posts' + field_query, function(err, res) {
+    get_recursive(postid, "posts", field_query, 10, function(err, res_posts) {
+        if (err || !res_posts) {
+            if (!res_posts) {
+                console.log("Err res_posts === null: ");
+                console.dir(res_posts);
+                //callback({"error": {"message": "No feed."}}, res_posts);
+            }
+            console.dir(err);
+            res.send({
+                "error": {
+                    "message": JSON.stringify(err)
+                }
+            });
+        } else {
+            res_posts.data = filter_information(res_posts.data);
+            var p = res_posts.data.length;
+            for (var i = 0; i < res_posts.data.length; i++) {
 
+                get_reactions(res_posts.data[i].id, "?fields=reactions.type(LIKE).limit(0).summary(true).as(like),reactions.type(LOVE).limit(0).summary(true).as(love),reactions.type(WOW).limit(0).summary(true).as(wow),reactions.type(HAHA).limit(0).summary(true).as(haha),reactions.type(SAD).limit(0).summary(true).as(sad),reactions.type(ANGRY).limit(0).summary(true).as(angry)", res_posts.data[i], function(err, result) {
+                    //reactions.push(res_reaction);
+                    //res_posts.data[i].reactions = res_reaction;
+                    //console.log(i + ": reactions");
+                    res_posts.data[i] = result;
+                    next();
+                });
+            }
+
+        }
+
+        function next() {
+            p--;
+            if (p === 0) final();
+        }
+
+        function final() {
+            savejson(postid, res_posts);
+            res.send(res_posts);
+        }
+    });
+}
+
+var get_recursive = function get_recursive(postid, field_query, subfield_query, MAX_DEPTH, callback) {
+
+    graph.get(postid + '/' + field_query + '/' + subfield_query, function(err, res) {
         if (err || !res) {
             if (!res) {
                 console.log("Error %s===null.", field_query);
@@ -37,18 +81,18 @@ var callback = function callback(req, res) {
                     }
                 }, res);
             }
-            //callback(err, res);
+            callback(err, res);
             return res;
         }
 
         var recurpaging = function recurpaging(res, depth, MAX_DEPTH, callback) {
-            
+
             if (depth >= MAX_DEPTH) {
                 console.log("[resursive paging: MAX_DEPTH");
                 console.log(field_query + ".length: " + data_query.data.length);
                 //savejson("data_query", data_query);
                 //console.log("data_query: " + data_query.data);
-                return ;
+                return;
             }
 
             if (res.data && res.paging && res.paging.next) {
@@ -75,10 +119,10 @@ var callback = function callback(req, res) {
                 console.log("[resursive paging: end --------------]");
                 console.log(field_query + ".length: " + data_query.data.length);
                 //savejson("data_query", data_query);
-                console.log("data_query: " + data_query.data.length );
+                //console.log("data_query: " + data_query.data.length);
                 //console.log(data_query);
-                savejson(postid,data_query);
-                return ;
+                callback(null, data_query);
+                return;
             }
         };
 
@@ -92,10 +136,103 @@ var callback = function callback(req, res) {
         console.log("page " + 1 + " " + field_query + ".length: " + data_query.data.length);
         recurpaging(res, 1, 10, callback);
 
-        return ;
+        return;
     });
+};
 
+function get_reactions(postid, subfield_query, post, callback) {
+    graph.get(postid + '/' + subfield_query, function(err, res) {
+        if (err || !res) {
+            if (!res) {
+                console.log("Error %s===null.", field_query);
+                callback({
+                    "error": {
+                        "message": "No sharedpost."
+                    }
+                }, res);
+            }
+            callback(err, res);
+            return res;
+        }
+        //console.log(res);
+        //console.log(post.likes);
+        post.reactions = {
+            "like": res.like.summary.total_count,
+            "love": res.love.summary.total_count,
+            "haha": res.haha.summary.total_count,
+            "wow": res.wow.summary.total_count,
+            "angry": res.angry.summary.total_count,
+            "sad": res.sad.summary.total_count
+        };
+        callback(null, post);
+        return;
+    });
+}
 
+function filter_information(postdata) {
+    console.log("posts length : " + postdata.length);
+
+    var data, result = [];
+
+    for (var i = 0; i < postdata.length; i++) {
+        if (postdata[i].created_time) {
+            if (postdata[i].shares) {
+                data = {
+                    "id": postdata[i].id,
+                    "object_id": postdata[i].object_id,
+                    "created_time": postdata[i].created_time,
+                    "type": postdata[i].type,
+                    "from": postdata[i].from,
+                    "shares": postdata[i].shares.count,
+                    "likes": postdata[i].reactions.summary.total_count,
+                    "reactions": null,
+                    "comments": postdata[i].comments.summary.total_count
+                };
+            } else {
+                data = {
+                    "id": postdata[i].id,
+                    "object_id": postdata[i].object_id,
+                    "created_time": postdata[i].created_time,
+                    "type": postdata[i].type,
+                    "from": postdata[i].from,
+                    "shares": 0,
+                    "likes": postdata[i].reactions.summary.total_count,
+                    "reactions": null,
+                    "comments": postdata[i].comments.summary.total_count
+                };
+            }
+
+        } else {
+            if (postdata[i].shares) {
+                data = {
+                    "id": postdata[i].id,
+                    "object_id": postdata[i].object_id,
+                    "created_time": postdata[i].updated_time,
+                    "type": postdata[i].type,
+                    "from": postdata[i].from,
+                    "shares": postdata[i].shares.count,
+                    "likes": postdata[i].reactions.summary.total_count,
+                    "reactions": null,
+                    "comments": postdata[i].comments.summary.total_count
+                };
+            } else {
+                data = {
+                    "id": postdata[i].id,
+                    "object_id": postdata[i].object_id,
+                    "created_time": postdata[i].updated_time,
+                    "type": postdata[i].type,
+                    "from": postdata[i].from,
+                    "shares": 0,
+                    "likes": postdata[i].reactions.summary.total_count,
+                    "reactions": null,
+                    "comments": postdata[i].comments.summary.total_count
+                };
+            }
+        }
+        result.push(data);
+    }
+
+    return result;
 }
 
 
