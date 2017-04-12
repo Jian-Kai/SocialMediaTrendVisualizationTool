2e28f4f060c0ca47ff7a716aaa2c88cecc4ba0f9
tony14256 <tony14256@gmail.com>
2017-04-12 13:41:21 +0800

    12th Month

---
+++ src/multiplot.js
@@ -0,0 +1,71 @@
+(function (multiplot) {
+    multiplot.plot = function (pos, classify, position) {
+        console.log(pos);
+        var start = 300;
+
+        xScale.range([10, 140]);
+        yScale.range([10, 140]);
+
+        for (var i = 0; i < pos.length; i++) {
+            d3.select("#svg2")
+                .append("rect")
+                .attr("x", pos[i].x)
+                .attr("y", pos[i].y)
+                .attr("width", 150)
+                .attr("height", 150)
+                .attr("fill", "white");
+
+            d3.select("#svg2")
+                .selectAll("#circle" + i)
+                .data(classify[i])
+                .enter()
+                .append("circle")
+                .attr("id", "#circle" + i)
+                .attr("cx", function (d) {
+                    return xScale(position[0][d.index]) + pos[i].x;
+                })
+
+                .attr("cy", function (d) {
+                    return yScale(position[1][d.index]) + pos[i].y;
+                })
+                .attr("r", 2)
+                .attr("fill", function () {
+                    return color(i);
+                })
+                .on("click", function (d) {
+                    console.log(d);
+                })
+
+        }
+    }
+
+
+
+    multiplot.multi = function (pagging, posts, width, height) {
+        console.log(pagging);
+        console.log(posts);
+        var length = posts.length;
+
+        var h = 150,
+            w = 150;
+
+        var pos = [];
+
+        for (var j = 0; j < 2; j++) {
+            for (var i = 0; i < length / 2; i++) {
+                var x, y;
+
+                x = i * (w + pagging) + pagging;
+                y = j * (h + pagging);
+
+                pos.push({
+                    "x": x,
+                    "y": y
+                });
+            }
+        }
+
+        return pos;
+    }
+
+}(window.multiplot = window.multiplot || {}));
