73c2631c8947cb1caa691a78c98b25290f2282a8
tony14256 <tony14256@gmail.com>
2017-06-15 10:59:35 +0800

    post link graph fileter

--- frequent.js
+++ frequent.js
@@ -9,7 +9,8 @@ var getData = function getData(fileName) {
         reject(err);
       }
       //call cutword function
-      resolve(cutword(JSON.parse(json).data));
+      var result = JSON.parse(json).data;
+      resolve(cutword(result));
     })
   });
 }
@@ -63,29 +64,98 @@ var count = function count(wordbuffer) {
   }
   //console.log(exist);
 
-  //fs.writeFile('./data/frequent.json', JSON.stringify({"key": exist}));
+  fs.writeFile('./data/frequent.json', JSON.stringify({
+    "key": exist
+  }));
   return exist;
 }
 
-var link = function link(data) {
+var link = function link(data, post) {
   var linkstruct = [];
- 
-  for(var i = 0 ; i < data.length; i++){
-    if(data[i].count > 1){
+
+  var monday = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].count > 1) {
       var key = data[i].posts;
-      for(var j = 0; j < key.length - 1; j++){
-        for(var k = j + 1; k < key.length; k++){
-          linkstruct.push({
-            "source": key[j],
-            "target": key[k],
-            "keyword": data[i].word
-          });
+      for (var j = 0; j < key.length; j++) {
+        for (var k = 0; k < key.length; k++) {
+          var sourcetime = new Date(post[key[j]].created_time).getMonth(),
+            targettime = new Date(post[key[k]].created_time).getMonth();
+            
+          if (sourcetime == targettime && 0 < (new Date(post[key[k]].created_time).getDate() - new Date(post[key[j]].created_time).getDate()) && (new Date(post[key[k]].created_time).getDate() - new Date(post[key[j]].created_time).getDate()) <= 7) {
+            linkstruct.push({
+              "source": key[j],
+              "target": key[k],
+              "keyword": data[i].word
+            });
+          } else if ((targettime - sourcetime) == 1 && 0 < (monday[sourcetime] - new Date(post[key[j]].created_time).getDate() + new Date(post[key[k]].created_time).getDate()) && (monday[sourcetime] - new Date(post[key[j]].created_time).getDate() + new Date(post[key[k]].created_time).getDate()) <= 7) {
+            linkstruct.push({
+              "source": key[j],
+              "target": key[k],
+              "keyword": data[i].word
+            });
+          }
+
+          /*
+          if (sourcetime < targettime && Math.abs(targettime - sourcetime) == 1) {
+            linkstruct.push({
+              "source": key[j],
+              "target": key[k],
+              "keyword": data[i].word
+            });
+          }
+          */
         }
       }
     }
   }
   console.log("tatal link: " + linkstruct.length);
-  //fs.writeFile('./data/link.json', JSON.stringify({"link": linkstruct}));
+  fs.writeFile('./data/link.json', JSON.stringify({
+    "link": linkstruct
+  }));
+  return linkstruct;
+}
+
+var filter = function filter(link) {
+  console.log("filter");
+  var Afilter = [{
+    "source": link[0].source,
+    "target": link[0].target,
+    "keyword": [link[0].keyword]
+  }];
+  //console.log(Afilter);
+  for (var i = 1; i < link.length; i++) {
+    var temp = link[i];
+    var add = true,
+      index;
+    for (var j = 0; j < Afilter.length; j++) {
+      var temp2 = Afilter[j];
+      if (temp.source == temp2.source && temp.target == temp2.target) {
+        index = j;
+        add = false;
+      } else if (temp.source == temp2.target && temp.target == temp2.source) {
+        index = j;
+        add = false;
+      }
+    }
+    if (add) {
+      Afilter.push({
+        "source": temp.source,
+        "target": temp.target,
+        "keyword": [temp.keyword]
+      })
+    } else {
+      Afilter[index].keyword.push(temp.keyword);
+    }
+  }
+
+
+  fs.writeFile('./data/filter.json', JSON.stringify({
+    "link": Afilter 
+  }));
+
+  console.log("done");
 }
 
 var context, word;
@@ -102,12 +172,13 @@ getData("./data/formatgreenpeace2016.json")
     console.log("total word : " + data.length);
     console.log("total post : " + context.length);
     word = count(data);
-    //console.log(word);
+    //console.log(context);
 
     return [context, word];
   })
   .then((data) => {
-    link(data[1]);
+    var linkstruct = link(data[1], data[0]);
+    filter(linkstruct);
   })
   .catch((error) => {
     console.log(error);
