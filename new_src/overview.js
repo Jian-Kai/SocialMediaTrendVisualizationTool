62898a63818b3b71cde6914bbe72f8837a6eb429
tony14256 <tony14256@gmail.com>
2017-07-11 16:05:21 +0800

    bezier curve

--- new_src/overview.js
+++ new_src/overview.js
@@ -56,16 +56,17 @@
 
         return Positions;
     }
-    overview.rander = function (position) {
+    overview.rander = function (position, time_curve) {
 
-        var x = (position)[0],
-            y = (position)[1];
+        var x = position[0],
+            y = position[1];
 
         var Xscale = d3.scaleLinear().domain([d3.min(x), d3.max(x)]).range([10, parseInt(overview_svg.style("width"), 10) - 10]);
         var Yscale = d3.scaleLinear().domain([d3.max(y), d3.min(y)]).range([10, parseInt(overview_svg.style("height"), 10) - 10]);
 
 
         overview_svg.append("g")
+            .attr("id", "posts")
             .selectAll("circle")
             .data(posts)
             .enter()
@@ -77,8 +78,138 @@
             .attr("cy", function (d, i) {
                 return Yscale(position[1][i]);
             })
-            .attr("r", 2.5)
-            .attr("fill", "red");
+            .attr("r", 4)
+            .attr("fill", "red")
+            .style("opacity", function (d, i) {
+                if (d.created_time.getMonth() == 5) {
+                    return 1;
+                } else {
+                    return 0.5;
+                }
+            })
+            .on("mouseover", function (d, i) {
+                console.log(i);
+                d3.select("#timecurve")
+                    .select("#link_" + d.post + "_" + (d.post + 1))
+                    .attr("stroke", "green")
+                    .attr("stroke-width", "4px");
 
+                d3.select("#timecurve")
+                    .select("#link_" + (d.post - 1) + "_" + d.post)
+                    .attr("stroke", "green")
+                    .attr("stroke-width", "4px");
+            })
+            .on("mouseout", function () {
+                d3.select("#timecurve")
+                    .selectAll("path")
+                    .attr("stroke", "black")
+                    .attr("stroke-width", "1px");
+            });
+
+
+        var curve = d3.line()
+            .x(function (d) {
+                return d.x
+            })
+            .y(function (d) {
+                return d.y
+            })
+            .curve(d3.curveBasis);
+
+
+        overview_svg.append("g")
+            .attr("id", "timecurve")
+            .selectAll("path")
+            .data(time_curve[5])
+            .enter()
+            .append("path")
+            .attr("id", function (d) {
+                return "link_" + d.start.post + "_" + d.end.post;
+            })
+            .attr("d", function (d) {
+                var start = [Xscale(position[0][d.start.post]), Yscale(position[1][d.start.post])],
+                    end = [Xscale(position[0][d.end.post]), Yscale(position[1][d.end.post])],
+                    pre = [Xscale(position[0][d.start.post - 1]), Yscale(position[1][d.start.post - 1])],
+                    pov = [Xscale(position[0][d.end.post + 1]), Yscale(position[1][d.end.post + 1])];
+                var m1 = [(end[0] - pre[0]) , (end[1] - pre[1])], m2 = [(start[0] - pov[0]) , (start[1] - pov[1])];
+                var D = Math.sqrt(Math.pow(start[0] - end[0], 2) + Math.pow(start[1] - end[1], 2)),
+                    MD1 = Math.sqrt(Math.pow(m1[0], 2) + Math.pow(m1[1], 2)),
+                    MD2 = Math.sqrt(Math.pow(m2[0], 2) + Math.pow(m2[1], 2));
+
+                    //console.log(D);
+
+                var line = [{
+                        "x": Xscale(position[0][d.start.post]),
+                        "y": Yscale(position[1][d.start.post])
+                    },
+                    {
+                        "x": start[0] + ((0.15 * D) / MD1) * m1[0],
+                        "y": start[1] + ((0.15 * D) / MD1) * m1[1]
+                    },
+                    {
+                        "x": end[0] + ((0.15 * D) / MD2) * m2[0],
+                        "y": end[1] + ((0.15 * D) / MD2) * m2[1]
+                    },
+                    {
+                        "x": Xscale(position[0][d.end.post]),
+                        "y": Yscale(position[1][d.end.post])
+                    }
+                ]
+                return curve(line);
+            })
+            .attr("stroke-width", "1px")
+            .attr("stroke", "black")
+            .attr("fill", "none");
+
+        /*
+        .attr("x1", function (d, i) {
+            return Xscale(position[0][d.start.post]);
+        })
+        .attr("y1", function (d, i) {
+            return Yscale(position[1][d.start.post]);
+        })
+        .attr("x2", function (d, i) {
+            return Xscale(position[0][d.end.post]);
+        })
+        .attr("y2", function (d, i) {
+            return Yscale(position[1][d.end.post]);
+        })
+         */
+
+    }
+
+
+    overview.timeline = function () {
+
+        var time = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ];
+
+        for (var i = 0; i < block_posts.length; i++) {
+            for (var j = 0; j < block_posts[i].length - 1; j++) {
+
+                time[i].push({
+                    "start": block_posts[i][j],
+                    "end": block_posts[i][j + 1],
+                });
+
+
+
+            }
+        }
+        //console.log(time);
+
+        return time;
     }
 })(window.overview = window.overview || {});
--- new_src/time_block.js
+++ new_src/time_block.js
@@ -1,5 +1,95 @@
 (function (timeblock) {
-    timeblock.multi = function(){
-        
+    timeblock.multi = function () {
+        var timeblock_width, timeblock_height, padding = 10;
+        var window_width = parseInt(timeblock_svg.style("width"), 10) - (padding * 2),
+            window_height = parseInt(timeblock_svg.style("height"), 10) - (padding * 2);
+
+        timeblock_width = (window_width - 5 * padding) / 6,
+            timeblock_height = (window_height - padding) / 2;
+
+        var position = [];
+
+        for (var i = 0; i < 12; i++) {
+            if (i < 6) {
+                position[i] = [padding + (i * (timeblock_width + padding)), padding];
+            } else {
+                position[i] = [padding + ((i - 6) * (timeblock_width + padding)), padding + (timeblock_height + padding)];
+            }
+        }
+
+        //console.log(position);
+        return {
+            "position": position,
+            "timeblock_width": timeblock_width,
+            "timeblock_height": timeblock_height
+        };
+    }
+    
+    timeblock.block = function (time_position, block_posts) {
+
+        var root = {
+            "x": time_position.timeblock_width / 2,
+            "y": time_position.timeblock_height / 2,
+            "r": time_position.timeblock_width / 2 - 10
+        };
+
+        timeblock_svg.selectAll("g")
+            .data(time_position.position)
+            .enter()
+            .append("g")
+            .attr("id", function (d, i) {
+                return "block" + i;
+            })
+            .append("rect")
+            .attr("x", function (d) {
+                return d[0];
+            })
+            .attr("y", function (d) {
+                return d[1];
+            })
+            .attr("rx", 10)
+            .attr("ry", 10)
+            .attr("width", time_position.timeblock_width)
+            .attr("height", time_position.timeblock_height)
+            .attr("fill", "white")
+            .style("stroke-width", "1px")
+            .style("stroke", "black");
+
+        timeblock_svg.selectAll("g")
+            .append("circle")
+            .attr("cx", function (d) {
+                return d[0] + (time_position.timeblock_width / 2);
+            })
+            .attr('cy', function (d) {
+                return d[1] + (time_position.timeblock_height / 2);
+            })
+            .attr("r", 20)
+            .attr("fill", "gray");
+
+
+        timeblock_svg.selectAll("g")
+            .append("g")
+            .attr("id", "posts")
+            .selectAll("circle")
+            .data(function (d, i) {
+                return block_posts[i];
+            })
+            .enter()
+            .append("circle")
+            .attr("id", function (d) {
+                return "block_posts" + d.id;
+            })
+            .attr("cx", function (d, j) {
+                return Math.cos((j * (360 / block_posts[d.created_time.getMonth()].length)) * (Math.PI / 180)) * root.r + time_position.position[d.created_time.getMonth()][0] + root.x;
+            })
+            .attr("cy", function (d, j) {
+                return Math.sin((j * (360 / block_posts[d.created_time.getMonth()].length)) * (Math.PI / 180)) * root.r + time_position.position[d.created_time.getMonth()][1] + root.y;
+            })
+            .attr("r", 2.5)
+            .attr("fill", function (d) {
+                //return (seven_color[d.post.created_time.getDay()]);
+                return "gray";
+            });
+
     }
 })(window.timeblock = window.timeblock || {});
