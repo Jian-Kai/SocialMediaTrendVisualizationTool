e99702f528f081fadce5e2b57f1008ef267b0ba1
tony14256 <tony14256@gmail.com>
2017-11-03 15:10:37 +0800

    run parameter 2

--- server/jieba.js
+++ server/jieba.js
@@ -1,27 +1,41 @@
 var nodejieba = require("nodejieba"),
     fs = require('fs');
+const os = require('os');
 
 var cut = function cut(posts, callback) {
     var data = posts;
     var result = [];
     //var test =[];
-    nodejieba.load({
-        dict: nodejieba.DEFAULT_DICT,
-        hmmDict: nodejieba.DEFAULT_HMM_DICT,
-        userDict: "d:/project/server/big_dict.utf8", //windows
-        //userDict: "/Users/guojiankai/Project/server/big_dict.utf8", //mac        
-        idfDict: nodejieba.DEFAULT_IDF_DICT,
-        stopWordDict: "d:/project/server/stop_dict.utf8" //windows
-        //stopWordDict: "/Users/guojiankai/Project/server/stop_dict.utf8" //mac    
-    });
+    if (os.platform() === "darwin") {
+        nodejieba.load({
+            dict: nodejieba.DEFAULT_DICT,
+            hmmDict: nodejieba.DEFAULT_HMM_DICT,
+            //userDict: "d:/project/server/big_dict.utf8", //windows
+            userDict: "/Users/guojiankai/Project/server/big_dict.utf8", //mac        
+            idfDict: nodejieba.DEFAULT_IDF_DICT,
+            //stopWordDict: "d:/project/server/stop_dict.utf8" //windows
+            stopWordDict: "/Users/guojiankai/Project/server/stop_dict.utf8" //mac    
+        });
+    } else {
+        nodejieba.load({
+            dict: nodejieba.DEFAULT_DICT,
+            hmmDict: nodejieba.DEFAULT_HMM_DICT,
+            userDict: "d:/project/server/big_dict.utf8", //windows
+            //userDict: "/Users/guojiankai/Project/server/big_dict.utf8", //mac        
+            idfDict: nodejieba.DEFAULT_IDF_DICT,
+            stopWordDict: "d:/project/server/stop_dict.utf8" //windows
+            //stopWordDict: "/Users/guojiankai/Project/server/stop_dict.utf8" //mac    
+        });
+    }
+
     var pattern = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）&;|{}【】‘；：”“'。，、？ ↵「」]");
     console.log(pattern);
     var p = 0,
         time = posts.length;
     for (var i = 0; i < data.length; i++) {
-        
+
         if (data[i].message) {
-            
+
             var temp = data[i].message;
             var str = "";
             for (var j = 0; j < temp.length; j++) {
@@ -47,7 +61,7 @@ var cut = function cut(posts, callback) {
                 "message_length": message_length,
                 "word": word
             });
-            
+
             next();
 
         } else {
--- test.js
+++ test.js
@@ -2,40 +2,56 @@ var fs = require("fs"),
     sw = require('stopword');
 
 
-/*
-fs.readFile("./data/CNN&Reuters/" + "CNN.json", function (err, json) {
-    if (err) throw err;
+let result = [];
 
-    data = JSON.parse(json).data;
-    let result = [];
-    console.log(data.length + " posts");
-    for (let i = 0; i < data.length; i++) {
+readdir(result);
 
-        var message = data[i].message.split(' ');
-        for (let j = 0; j < message.length; j++) {
-            if (message[j].slice(0, 4) !== "http")
-                message[j] = message[j].replace(/\W|_/g, '');
-        }
-        //console.log(message);
-        var word = sw.removeStopwords(message);
-        //console.log(word);
+function readdir(result) {
+    return new Promise((resolve, reject) => {
+        fs.readdir("./data/pos", function (err, list) {
+            if (err)
+                reject("No Find Dir");
 
-        result.push({
-            "id": data[i].id,
-            "created_time": data[i].created_time,
-            "type": data[i].type,
-            "message": data[i].message,
-            "from": data[i].from,
-            "shares": data[i].shares,
-            "likes": data[i].reactions.like + data[i].reactions.love + data[i].reactions.haha + data[i].reactions.wow + data[i].reactions.sad + data[i].reactions.angry,
-            "reactions": data[i].reactions,
-            "comments": data[i].comments,
-            "message_length": message.length,
-            "word": word
-        });
+            resolve(filename(list, result));
+        })
+    })
+}
 
+async function readfile(name, result) {
+    return new Promise((resolve, reject) => {
+        fs.readFile("./data/pos/" + name, function (err, json) {
+            if (err)
+                reject("no file");
+
+            data = JSON.parse(json);
+            result.push(data);
+            
+            resolve(result);
+
+        })
+    })
+}
+
+
+
+async function filename(list, result) {
+    console.log(list);
+    for (let i = 1; i < list.length; i++) {
+        result = await readfile(list[i], result);
     }
 
-    //console.log(result);
+    await next();
 
-})*/
+    function next() {
+        console.log(result.length);
+
+        const content = JSON.stringify(result);
+        fs.writeFile("./data/pos/position.json", content, 'utf8', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("The file was saved!");
+        });
+    }
+}
